ee6c0eecf8f38b3b3ce82d17b906f492fab77d22
Gopika1678 <107744693+Gopika1678@users.noreply.github.com>
2022-08-22 17:52:56 +0530

    Create CalculateDiscount.js

---
+++ src/shared/CalculateDiscount.js
@@ -0,0 +1,8 @@
+export const calcDiscount = (actual_price, discount) => {
+    const final_price =
+      Number(actual_price) - Number((actual_price * discount) / 100)
+  
+    return Number(final_price)
+  }
+  
+  

2a7e86cd2ee57a12d21f6a8c0f4da1347395a218
Gopika1678 <107744693+Gopika1678@users.noreply.github.com>
2022-08-22 17:53:32 +0530

    Create Formatter.js

---
+++ src/shared/Formatter.js
@@ -0,0 +1,8 @@
+export function moneyConverter(input) {
+    if (input !== null && typeof input !== 'undefined') {
+      return input.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+    }
+    return String(input)
+  }
+  
+  
